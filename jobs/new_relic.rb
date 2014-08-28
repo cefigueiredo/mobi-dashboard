f31008c88aaa0d24b84a2b508f74d7d302702601
Carlos Figueiredo <carlos.figueiredo87@gmail.com>
2014-08-28 08:21:43 +0100

    #2 Carregar dados da NewRelic e registrar em Log no banco

--- jobs/new_relic.rb
+++ jobs/new_relic.rb
@@ -1,19 +1,20 @@
 require 'json'
+require 'rest-client'
 require 'pry'
 
-servers_raw = File.read('JSON_SERVIDORES.json')
-servers = JSON.parse(servers_raw)['servers'].map { |srv| srv}
+
+def servers_check
+  resource = RestClient::Resource.new('https://api.newrelic.com/v2')
+  Account.all.map do |account|
+    result = resource['servers.json'].get('X-Api-Key' => account.api_key)
+    AccountCheck.create( :account => account,
+                         :status  => result.headers[:status],
+                         :date    => DateTime.parse(result.headers[:date]))
+    JSON.parse(result)['servers'] || []
+  end
+end
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  binding.pry
-  servers = servers.map do |srv|
-    if srv['summary']
-      srv['summary']['cpu'] = rand
-      srv['summary']['memory'] = srv['summary']['memory'] * rand(10)
-      srv['summary']['disk_io'] = srv['summary']['disk_io'] * rand(10)
-    end
-    srv
-  end
-
+  servers = servers_check.flatten
   send_event('my_widget', { servers: servers })
 end
